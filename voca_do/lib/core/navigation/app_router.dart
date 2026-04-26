9680f5c1779e3df0ccd7c73f5de322d0e201217f
Leen2025 <leeeen4@hotmail.com>
2026-04-26 05:43:10 +0300

    fix merge conflict

--- voca_do/lib/core/navigation/app_router.dart
+++ voca_do/lib/core/navigation/app_router.dart
@@ -21,7 +21,7 @@ import 'package:voca_do/features/task_viewer/presentation/pages/task_list_screen
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: Routes.login,
+    initialLocation: Routes.taskViewer,
     routes: [
       GoRoute(
         path: Routes.login,
